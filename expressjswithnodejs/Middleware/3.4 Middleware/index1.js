d858aa119ee3674456a5e7fb66c91dc85bf03977
Shams1412 <62950014+Shams1412@users.noreply.github.com>
2023-08-31 02:18:12 +0500

    middleware body parser,morgan,custom middleware

--- expressjswithnodejs/Middleware/3.4 Middleware/index1.js
+++ expressjswithnodejs/Middleware/3.4 Middleware/index1.js
@@ -1,3 +1,43 @@
+// import express from "express";
+// import {dirname} from "path";
+// import {fileURL} from "url";
+// const app = express();
+// const port = 3000;
+// const dirnamepath = dirname(fileURL(import.meta.url)); 
+
+// app.get("/", (req, res) => {
+//   console.log(dirnamepath + "/public/index.html");
+//   res.sendFile(dirnamepath + "/public/index.html");
+// })
+
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
